fb6a188352b2dcd60b7b6daceaab2db068f91a99
George <2392568162@qq.com>
2019-07-20 18:07:22 +0800

    new

---
+++ 003.py
@@ -0,0 +1,14 @@
+def Find(matrix, rows, columns, number):
+    found = False
+    if matrix != None and rows > 0 and columns > 0:
+        row = 0
+        column = columns -1
+        while row < rows and column >= 0:
+            if matrix[row*column + column] == number:
+                found = True
+                break
+            elif matrix[row*column + column] > number:
+                column -= 1
+            else:
+                row += 1
+    return found
---
+++ 004.py
@@ -0,0 +1,30 @@
+def ReplaceBlank(string, length):
+    if string == None or length <= 0:
+        return
+
+    originalLength = len(string)
+    numberOfBlank = 0
+    i = 0
+    while i != originalLength:
+        if string[i] == " ":
+            numberOfBlank += 1
+        i += 1
+
+    newLength = originalLength + numberOfBlank *2
+    if newLength > length :
+        return
+    indexOfOriginal = originalLength
+    indexOfNew = newLength
+    while indexOfOriginal >= 0 and indexOfNew > indexOfOriginal:
+        if string[indexOfOriginal] == " ":
+            string[indexOfNew] = '0'
+            indexOfNew -= 1
+            string[indexOfNew] = '2'
+            indexOfNew -= 1
+            string[indexOfNew] = '%'
+            indexOfNew -= 1
+        else:
+            string[indexOfNew] = string[indexOfOriginal]
+            indexOfNew -= 1
+        indexOfOriginal -= 1
+
