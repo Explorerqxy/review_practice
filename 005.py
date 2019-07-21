25f4d821b999ac4fe1bd2160d1f8fa18727e508f
George <2392568162@qq.com>
2019-07-21 11:29:32 +0800

    new

---
+++ 005.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+def PrintListReversingly_Iteratively(pHead):
+    nodes = []
+    pNode = pHead
+    while pHead != None:
+        nodes.append(pNode.val)
+        pNode = pNode.next
+    while not nodes:
+        pNode = nodes.pop()
+        print(pNode)
+
+def PrintListReversingly_recursively(pHead):
+    if pHead != None:
+        if pHead.next != None:
+            PrintListReversingly_recursively(pHead.next)
+        print(pHead.val)
