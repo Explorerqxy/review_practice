6bf15442fd7cd3cd13c9bb4d4a0f833f3ece75f1
George <2392568162@qq.com>
2019-07-31 15:12:03 +0800

    mdf

--- 041.py
+++ 041.py
@@ -14,7 +14,26 @@ def getMaxValue_solution1(values, rows, cols):
     maxValue = maxValues[-1][-1]
     return maxValue
 
+def getMaxValue_solution2(values, rows, cols):
+    if values == None or rows <= 0 or cols <= 0:
+        return 0
+    maxValues = [0 for _ in range(cols)]
+    for i in range(rows):
+        for j in range(cols):
+            up = 0
+            left = 0
+            if i > 0:
+                up = maxValues[j]
+            if j > 0:
+                left = maxValues[j -1]
+            maxValues[j] = max(up, left) + values[i * cols + j]
+    maxValue = maxValues[-1]
+    return maxValue
+
+
 if __name__ == '__main__':
     a = [1,10,3,8,12,2,9,6,5,7,4,11,3,7,16,5]
     t = getMaxValue_solution1(a,4,4)
+    b = getMaxValue_solution2(a,4,4)
     print(t)
+    print(b)
